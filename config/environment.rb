687172f79fa5e8047496f1ab975ba66548e92d6e
Espen <espen.taftoe@gmail.com>
2017-05-14 11:05:09 +0000

    Fixed sendgrid v2

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
-ActionMailer::Base.smtp_setting = {
+ActionMailer::Base.smtp_settings = {
     :adress => 'smtp.sendgrid.net',
     :port => '597',
     :authentication => :plain,
